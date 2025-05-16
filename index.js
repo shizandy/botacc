c1eab4aee3464ced648951be04cc4a11e2f5c131
shizandy <shizxyandy+@gmail.com>
2025-05-16 15:22:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Configuration MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo:YbtEJgyPWxZPGFqfKjHhvHjZLBBbIQnl@shortline.proxy.rlwy.net:56805';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://maxiiiii:yooo6916@cluster0.qsehi0c.mongodb.net/?retryWrites=true&w=majority';
 const DB_NAME = 'bot_auth';
 const COLLECTION_NAME = 'auth_instances';
 
